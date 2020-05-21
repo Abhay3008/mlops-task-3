acebaea0df7d45957ceb09ba39dffb466cf7c768
Abhay3008 <54666019+Abhay3008@users.noreply.github.com>
2020-05-21 15:31:46 +0530

    Add files via upload

---
+++ fruitclassifier.py
@@ -0,0 +1,120 @@
+from keras.applications import VGG16
+
+img_rows = 64
+img_cols = 64 
+
+vgg16 = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+for layer in vgg16.layers:
+    layer.trainable = False
+def addTopModel(bottom_model, num_classes, D=256):
+    """creates the top or head of the model that will be 
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dropout(0.3)(top_model)
+    top_model = Dense(num_classes, activation = "softmax")(top_model)
+    return top_model
+from keras.applications import VGG16
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = '/root/Desktop/fruits-360/Training/'
+validation_data_dir = '/root/Desktop/fruits-360/validation/'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Re-loads the VGG16 model without the top or FC layers
+vgg16 = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+# Freeze layers
+for layer in vgg16.layers:
+    layer.trainable = False
+    
+# Number of classes in the Flowers-17 dataset
+num_classes = 15
+
+FC_Head = addTopModel(vgg16, num_classes)
+
+model = Model(inputs=vgg16.input, outputs=FC_Head)
+
+print(model.summary())
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+                   
+checkpoint = ModelCheckpoint("flowers_vgg_64.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 5,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
+                              factor = 0.2,
+                              patience = 3,
+                              verbose = 1,
+                              min_delta = 0.00001)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint, reduce_lr]
+
+# Note we use a very small learning rate 
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.0001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 20
+batch_size = 32
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+model.save("fruitclassifier.h5")
+
